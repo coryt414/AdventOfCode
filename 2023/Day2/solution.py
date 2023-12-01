c94fca9d4e52c9783730862d293f5637135b97a2
Cory Thomas <coryt414@gmail.com>
2023-12-01 11:12:53 -0500

    Set up day2

---
+++ 2023/Day2/solution.py
@@ -0,0 +1,17 @@
+from os import path
+
+
+"""https://github.com/benediktwerner/AdventOfCode/blob/master/2023/day01/sol.py"""
+
+def try_parse(line: str, numbers: dict[str, int]) -> int | None:
+
+    return value
+
+def solve(lines: list[str], numbers: dict[str, int]):
+
+    return result
+
+with open(path.join(path.dirname(__file__), "input.txt")) as f:
+    lines = f.read().splitlines()
+    print("Part 1:", solve(lines, xxxxx))
+    print("Part 2:", solve(lines, xxxxx | yyyyy))
