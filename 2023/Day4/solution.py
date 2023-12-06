6968c13d982492879afd35c29abbcb70adfc6342
Cory Thomas <coryt414@gmail.com>
2023-12-06 12:41:28 -0500

    Complete Day4

--- 2023/Day4/solution.py
+++ 2023/Day4/solution.py
@@ -31,9 +31,9 @@ def solve2(lines: list[str]):
     final_cards = []
     total = 0
     line = ""
-    for i in range(len(lines)):
+    for item in (lines):
         round_score = 0
-        line = lines[i].split(":")
+        line = item.split(":")
         line = line[1].split("|")
         winning_numbers = line[0].strip(" ")
         winning_numbers = winning_numbers.split(" ")
@@ -48,31 +48,14 @@ def solve2(lines: list[str]):
                 #round_score += 1
             if number in my_numbers:
                 round_score += 1
-        for j in range(i + 1, i + 1 + round_score):
+        index = lines.index(item)
+        for j in range(index + 1, index + 1 + round_score):
             lines.append(lines[j])
-        lines.sort()
         total += round_score
+    lines.sort()
 
-    total = 0
-    for line in final_cards:
-        round_score = 0
-        line = line.split(":")
-        line = line[1].split("|")
-        winning_numbers = line[0].strip(" ")
-        winning_numbers = winning_numbers.split(" ")
-        winning_numbers = [x for x in winning_numbers if x!='']
-        my_numbers = line[1].strip(" ")
-        my_numbers = my_numbers.split(" ")
-        my_numbers = [x for x in my_numbers if x!='']
-
-        for number in winning_numbers:
-            if number in my_numbers and round_score == 0:
-                round_score += 1
-            elif number in my_numbers and round_score > 0:
-                round_score = round_score * 2
-        total += round_score
-    print(len(lines))
-    return total
+    answer = len(lines)
+    return answer
 
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
     lines = f.read().splitlines()
---
+++ 2023/Day5/solution.py
@@ -0,0 +1,13 @@
+from os import path
+
+"""Example layout from github below"""
+"""https://github.com/benediktwerner/AdventOfCode/blob/master/2023/day01/sol.py"""
+
+def solve(lines: list[str]):
+    
+    return
+
+with open(path.join(path.dirname(__file__), "input.txt")) as f:
+    lines = f.read().splitlines()
+    print("Part 1:", solve(lines))
+    #print("Part 2:", solve(lines, max))
